200ac81839338a0d900993e103d1d5d7ffdcb6a4
apekshagangurde <100061307+apekshagangurde@users.noreply.github.com>
2023-12-29 12:13:10 +0530

    Create  String Compression.cpp

---
+++ String Compression.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <string>
+
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int i = 0;
+        int ansIndex = 0;
+        int n = chars.size();
+
+        while (i < n) {
+            int j = i + 1;
+
+            while (j < n && chars[i] == chars[j]) {
+                j++;
+            }
+
+            chars[ansIndex] = chars[i];
+            ansIndex++;
+
+            int count = j - i;
+            if (count > 1) {
+               string countStr = to_string(count);
+                for (char ch : countStr) {
+                    chars[ansIndex] = ch;
+                    ansIndex++;
+                }
+            }
+
+            i = j;
+        }
+
+        return ansIndex;
+    }
+};
